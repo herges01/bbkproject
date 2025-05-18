07e27c7d11119c71a23e9765e631dff8189c1c16
Hashim Ergesous <herges01@student.bbk.ac.uk>
2025-05-18 02:26:58 +0100

    Updated Home View

--- views/home.php
+++ views/home.php
@@ -1,12 +1,15 @@
 <?php
 $headTitle = 'PHP Cwk2 Home Page';
 $viewHeading = htmlHeading('Home Page View',2);
-$content = htmlParagraph("Users in the database:");
 #add your code here to display users stored in the database
 $tableName = "projectUsersTable";
+$content = htmlHeading("This is a site designed to help you manage your finances. You can add, delete, and update your transactions, and view them in a table or graph.",3);
+$content .= htmlParagraph("Users in the database: (Only one user is shown for testing purposes)");
 $data = UserDataFetcher($pdo,$db,$tableName);
-foreach ($data as $info){
-	$content .= htmlParagraph('username: ' . $info['Username'] . ' , ' . 'password: ' . $info['Password']);
+if(count($data) > 0){	
+    $content .= htmlParagraph('Username: ' . $data[0]['Username'] . ' ' . 'Password: ' . $data[0]['Password']);
 }
-
+else{
+    $content .= htmlParagraph("No users found");
+}
 ?>
